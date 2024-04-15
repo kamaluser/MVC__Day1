61c7e4c3c2919fde684a83c9c7eae97baef45e5e
Kamal Huseynaliyev <kamalrh@code.edu.az>
2024-04-16 00:05:26 +0400

    Initial commit

---
+++ MVC-Day1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Day1.Models;
+
+namespace MVC_Day1.Controllers
+{
+    public class HomeController:Controller
+    {
+        private static List<Product> products = new List<Product> 
+        {
+        new Product { Id = 1, Name = "Product 1", Price = 9.99 },
+        new Product { Id = 2, Name = "Product 2", Price = 5.49 },
+        new Product { Id = 3, Name = "Product 3", Price = 15.99 }
+        };
+
+
+        public ViewResult Index()
+        {
+            ViewBag.Products = products;
+            return View(products);
+        }
+
+        public ViewResult Detail(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+
+            ViewBag.Product = product;
+
+
+            return View(product);
+        }
+    }
+}
